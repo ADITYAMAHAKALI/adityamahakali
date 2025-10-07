3e26bd8fd1558ef32e359c80c7bf13d3ac1d8634
adityamahakali <adityamahakali@gmail.com>
2025-10-07 23:53:49 +0530

    fixed error

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,22 @@
-import { getBlogPosts } from 'app/blog/utils'
+// import { getBlogPosts } from 'app/blog/utils'
 
 export const baseUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL || 'https://adityamahakali.vercel.app';
 // console.log('baseUrl', baseUrl)
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }))
+  // let blogs = getBlogPosts().map((post) => ({
+  //   url: `${baseUrl}/blog/${post.slug}`,
+  //   lastModified: post.metadata.publishedAt,
+  // }))
 
-  let routes = ['', '/blog'].map((route) => ({
+  // let routes = ['', '/blog'].map((route) => ({
+  //   url: `${baseUrl}${route}`,
+  //   lastModified: new Date().toISOString().split('T')[0],
+  // }))
+  let routes = [''].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
-
-  return [...routes, ...blogs]
+  return routes;
+  // return [...routes, ...blogs]
 }
